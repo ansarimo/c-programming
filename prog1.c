dd9105af55fef9ec52b72c588f0f5d307312e896
ansarimo <writetofarooq@gmail.com>
2024-08-14 08:24:03 +0530

    Added Thanks

--- prog1.c
+++ prog1.c
@@ -3,5 +3,5 @@ int main( )
 {
     printf("Hello World");
     printf("Welcome to C programming. It will be fun.");
-    printf("Learning C is going to be a fun.");
+    printf("Learning C is going to be a fun. Thanks!!");
 }
